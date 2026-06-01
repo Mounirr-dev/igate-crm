ab86056ffbcc4159dc937832bd8d9eecd5dd2df4
Mounir <erramimounir49@gmail.com>
2026-06-01 01:12:37 +0100

    Connect frontend to render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://TON-BACKEND.onrender.com/api"
+  baseURL: "https://igate-crm.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
